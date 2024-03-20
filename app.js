53590229b05cb7a3bc4337acaf44d3554b743b92
Mr-Hasan-Raza <Mr.HasanRaza@otlook.com>
2024-03-20 23:30:20 +0500

    filetobesubmitt

---
+++ app.js
@@ -0,0 +1,33 @@
+// QUESTION 01
+let que01 = [];
+
+// QUESTION 02
+// NOT LEARN 
+
+// QUESTION 03
+let que03 = ['Muhammad Hasan Raza'];
+console.log(que03);
+
+// QUESTION 04
+let que04 = [21];
+console.log(que04);
+
+// QUESTION 05
+let que05 = [true];
+console.log(que05);
+
+// QUESTION 06
+let que06 = ['Muhammad Hasan Raza' , 21 , true];
+console.log(que06);
+
+// QUESTION 07
+let que07 = ['Jazz' , 'Warid' , 'Telenor' , 'Ufone' , 'Mobilink'];
+console.log(que07);
+
+// QUESTION 08
+let que08 = document.querySelector('#que08');
+    que08.innerHTML += `<li>SSC , HSC , BCS , BS , BCOM , MS , M. Phil , PhD</li>`
+
+// QUESTION 09
+let que09 = ['Avengers: Age of Ultron' , 'Spectre' , 'Jurassic World' , 'Inside Out'];
+console.log(que09);
